ba61096c38bc482e358012fdcece0d4742c694e3
Riccardo <ricca.pit@hotmail.it>
2026-07-20 15:17:58 +0200

    Add VAE

---
+++ src/latent_diffusion/autoencoder/vae.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+from diffusers import AutoencoderKL
+
+class FrozenVAE(nn.Module):
+  def __init__(self, vae_name: str):
+    """
+    Instantiate a pretrained VAE from vae_name, freezes it and encodes images or decodes latents applying the vae's scaling factor
+    args:
+      vae_name = name of pretrained vae - "stabilityai/sd-vae-ft-mse"
+    """
+    super().__init__()
+    self.vae = AutoencoderKL.from_pretrained(vae_name)
+    for param in self.vae.parameters():
+      if param.requires_grad:
+        param.requires_grad = False
+    
+    self.vae.eval()
+
+  @torch.no_grad()
+  def encode(self, image: torch.Tensor):
+    """
+    Encodes an image tensor into a latent representation automatically scaled by the VAE's scaling factor
+    args:
+      image (torch.Tensor)
+    out:
+      latents = latent scaled representation
+    """
+    posterior = self.vae.encode(image).latent_dist
+    latents = posterior.sample()
+    latents = latents * self.vae.config.scaling_factor
+    return latents
+
+  @torch.no_grad()
+  def decode(self, latent: torch.Tensor):
+    """
+    Decodes a latent into an image tensor automatically de-scaled by the VAE's scaling factor
+    args:
+      latents = latent scaled representation
+    out:
+      image (torch.Tensor)
+    """
+    latent = latent / self.vae.config.scaling_factor
+    image = self.vae.decode(latent).sample
+    return image
+  
