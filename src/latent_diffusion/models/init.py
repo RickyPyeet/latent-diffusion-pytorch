e82161ba1d18a235dd4abf1782de3c5cb45066cc
Riccardo <ricca.pit@hotmail.it>
2026-07-20 15:24:27 +0200

    Add conv initialization

---
+++ src/latent_diffusion/models/init.py
@@ -0,0 +1,9 @@
+
+import torch
+from torch import nn
+# Helper function to initialize weights and bias in a new conv block
+def init_conv(m, mode = 'fan_in', nonlinearity = 'relu'):
+  if isinstance(m, nn.Conv2d):
+    nn.init.kaiming_normal_(m.weight, mode = mode, nonlinearity = nonlinearity)
+    if m.bias is not None:
+      nn.init.zeros_(m.bias)
